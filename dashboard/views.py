20d7d8816153a1120002c218f83c42765f4423b7
steve <steviewez@gmail.com>
2017-08-07 22:33:23 +0300

    set iebc project

---
+++ dashboard/views.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from django.views.generic import TemplateView
+import requests
+
+
+class DashboardTemplateView(TemplateView):
+    template_name = 'dashboard/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(DashboardTemplateView, self).get_context_data(**kwargs)
+        url = "https://apitest.secured.vote/jsons/round1/results/Kenya_Elections_Presidential/1/info.json"
+        headers = {
+            'content-type': "application/json",
+        }
+        response = requests.request("GET", url, headers=headers)
+
+        data = response.json()
+        context['polling'] = data['progress']
+        context['parties'] = data['results']['parties']
+        print context
+        return context
